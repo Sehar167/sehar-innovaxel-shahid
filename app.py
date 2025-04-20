f21408fb261c304f5dbf71d702f7e899c8d79e70
Sehar167 <seharshah932@gmail.com>
2025-04-20 16:54:32 +0500

    Add URL validation

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 import random
 import string
 import time
+import re
+
+def is_valid_url(url):
+    regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.))' # domain...
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(regex, url) is not None
 
 # Initialize Flask and SQLite database
 app = Flask(__name__)
